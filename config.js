bc3ad854f153c99439c18ed0a3bce0121a1a16e5
Izabella <manansoni.soni77@gmail.com>
2022-06-13 13:35:51 +0530

    mongouri string

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    mongouri: process.env.mongouri,
+    mongouri: String(process.env.mongouri),
     PORT: process.env.PORT || 5000,
 }
