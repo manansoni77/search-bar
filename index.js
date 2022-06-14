321fa9d9c398458ce2804e5813cd4cafd8eb8d6e
Izabella <manansoni.soni77@gmail.com>
2022-06-14 13:35:48 +0530

    Updated node modules

--- index.js
+++ index.js
@@ -2,20 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { PORT, mongouri } = require('./config')
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const userRoutes = require('./routes/api/user')
 const path = require('path')
 
 const app = express();
 
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+    extended: false
 }));
 
-const static_path = path.join(__dirname, "client");;
-console.log(static_path)
+const static_path = path.join(__dirname, "client");
 app.use(express.static(static_path));
 app.use('/api/user', userRoutes)
 
